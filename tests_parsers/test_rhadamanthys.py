1cc2a65fb41bd523bcb8beb67a4dacd91b6cf481
Yung Binary <93540406+YungBinary@users.noreply.github.com>
2025-01-03 11:06:20 +0000

    Add test

---
+++ tests_parsers/test_rhadamanthys.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2010-2015 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+from cape_parsers.CAPE.core.Rhadamanthys import extract_config
+
+
+def test_lumma():
+    with open("tests/data/malware/b70519d6094fa55da2b86d897be0040ee84d0b3ef61ab4fd8d08a20628a67497", "rb") as data:
+        conf = extract_config(data.read())
+        assert conf == {
+            "Reexecution_delay": 0,
+            "C2": [
+                "https://51.75.171.9:5151/9640d96bbead45f349f3ab9/Xteam30.api"
+            ]
+        }
