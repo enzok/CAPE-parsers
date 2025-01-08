b2b55684b266f0aedc44640e42b4e6fcedfec7d9
Yung Binary <93540406+YungBinary@users.noreply.github.com>
2025-01-08 17:43:10 +0000

    Grab botnet id from stealc samples

--- cape_parsers/CAPE/community/Stealc.py
+++ cape_parsers/CAPE/community/Stealc.py
@@ -1,6 +1,7 @@
 import struct
 import pefile
 import yara
+import json
 from contextlib import suppress
 
 
@@ -61,54 +62,63 @@ def extract_config(data):
                 uri = line
         if domain and uri:
             config_dict.setdefault("C2", []).append(f"{domain}{uri}")
+            return config_dict
     except Exception:
         pass
 
     # Try with new method
-    if not config_dict.get("C2"):
-        with suppress(Exception):
-            #config_dict["Strings"] = []
-            pe = pefile.PE(data=data, fast_load=False)
-            image_base = pe.OPTIONAL_HEADER.ImageBase
-            domain = ""
-            uri = ""
-            last_str = ""
-            for match in yara_scan(data):
-                rule_str_name, str_decode_offset = match
-                str_size = int(data[str_decode_offset + 1])
-                # Ignore size 0 strings
-                if not str_size:
-                    continue
 
-                if rule_str_name.startswith("$decode"):
-                    key_rva = data[str_decode_offset + 3 : str_decode_offset + 7]
-                    encoded_str_rva = data[str_decode_offset + 8 : str_decode_offset + 12]
-                    #dword_rva = data[str_decode_offset + 21 : str_decode_offset + 25]
+    #config_dict["Strings"] = []
+    pe = pefile.PE(data=data, fast_load=False)
+    image_base = pe.OPTIONAL_HEADER.ImageBase
+    domain = ""
+    uri = ""
+    botnet_id = ""
+    last_str = ""
+    for match in yara_scan(data):
+        try:
+            rule_str_name, str_decode_offset = match
+            str_size = int(data[str_decode_offset + 1])
+            # Ignore size 0 strings
+            if not str_size:
+                continue
 
-                key_offset = pe.get_offset_from_rva(struct.unpack("i", key_rva)[0] - image_base)
-                encoded_str_offset = pe.get_offset_from_rva(struct.unpack("i", encoded_str_rva)[0] - image_base)
-                #dword_offset = struct.unpack("i", dword_rva)[0]
-                #dword_name = f"dword_{hex(dword_offset)[2:]}"
+            if rule_str_name.startswith("$decode"):
+                key_rva = data[str_decode_offset + 3 : str_decode_offset + 7]
+                encoded_str_rva = data[str_decode_offset + 8 : str_decode_offset + 12]
+                #dword_rva = data[str_decode_offset + 21 : str_decode_offset + 25]
 
-                key = data[key_offset : key_offset + str_size]
-                encoded_str = data[encoded_str_offset : encoded_str_offset + str_size]
-                decoded_str = xor_data(encoded_str, key).decode()
-                #config_dict["Strings"].append({dword_name : decoded_str})
+            key_offset = pe.get_offset_from_rva(struct.unpack("i", key_rva)[0] - image_base)
+            encoded_str_offset = pe.get_offset_from_rva(struct.unpack("i", encoded_str_rva)[0] - image_base)
+            #dword_offset = struct.unpack("i", dword_rva)[0]
+            #dword_name = f"dword_{hex(dword_offset)[2:]}"
 
-                if last_str in ("http://", "https://"):
-                    domain += decoded_str
-                elif decoded_str in ("http://", "https://"):
-                    domain = decoded_str
-                elif "http" in decoded_str and "://" in decoded_str:
-                    domain = decoded_str
-                elif decoded_str.startswith("/") and decoded_str[-4] == ".":
-                    uri = decoded_str
+            key = data[key_offset : key_offset + str_size]
+            encoded_str = data[encoded_str_offset : encoded_str_offset + str_size]
+            decoded_str = xor_data(encoded_str, key).decode()
+            #config_dict["Strings"].append({dword_name : decoded_str})
 
-                last_str = decoded_str
+            if last_str in ("http://", "https://"):
+                domain += decoded_str
+            elif decoded_str in ("http://", "https://"):
+                domain = decoded_str
+            elif "http" in decoded_str and "://" in decoded_str:
+                domain = decoded_str
+            elif uri == "" and decoded_str.startswith("/") and decoded_str[-4] == ".":
+                uri = decoded_str
+            elif last_str[0] == '/' and last_str[-1] == '/':
+                botnet_id = decoded_str
 
-                if domain and uri:
-                    config_dict.setdefault("C2", []).append(f"{domain}{uri}")
-                    return config_dict
+            last_str = decoded_str
+
+        except Exception:
+            continue
+        
+    if domain and uri:
+        config_dict.setdefault("C2", []).append(f"{domain}{uri}")
+
+    if botnet_id:
+        config_dict.setdefault("Botnet ID", botnet_id)
 
     return config_dict
 
