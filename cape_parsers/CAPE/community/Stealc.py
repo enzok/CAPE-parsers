eb9f5fd095887a109f95c224c4748674a5ca7f8b
Kevin O'Reilly <kevoreilly@gmail.com>
2025-08-05 14:27:31 +0100

    Stealc V2

--- cape_parsers/CAPE/community/Stealc.py
+++ cape_parsers/CAPE/community/Stealc.py
@@ -1,31 +1,35 @@
 import struct
 import pefile
 import yara
+from contextlib import suppress
 
 
-# Hash = 619751f5ed0a9716318092998f2e4561f27f7f429fe6103406ecf16e33837470
+# V1 hash = 619751f5ed0a9716318092998f2e4561f27f7f429fe6103406ecf16e33837470
+# V2 hash = 2f42dcf05dd87e6352491ff9d4ea3dc3f854df53d548a8da0c323be42df797b6 (32-bit payload)
+# V2 hash = 8301936f439f43579cffe98e11e3224051e2fb890ffe9df680bbbd8db0729387 (64-bit payload)
 
-RULE_SOURCE = """rule StealC
+RULE_SOURCE = """
+rule StealC
 {
     meta:
         author = "Yung Binary"
     strings:
-        $decode_1 = {
-            6A ??
-            68 ?? ?? ?? ??
-            68 ?? ?? ?? ??
-            E8 ?? ?? ?? ??
-        }
-        $decode_2 = {
-            6A ??
-            68 ?? ?? ?? ??
-            68 ?? ?? ?? ??
-            [0-5]
-            E8 ?? ?? ?? ??
-        }
+        $decode_1 = {6A ?? 68 [4] 68 [4] E8}
+        $decode_2 = {6A ?? 68 [4] 68 [4] [0-5] E8}
     condition:
         any of them
-}"""
+}
+rule StealcV2
+{
+    meta:
+        author = "kevoreilly"
+    strings:
+        $botnet32 = {AB AB AB AB 89 4B ?? C7 43 ?? 0F 00 00 00 88 0B A0 [4] EB 12 3C 20 74 0B 0F B6 06 8B CB 50 E8}
+        $botnet64 = {0F 11 01 48 C7 41 ?? 00 00 00 00 48 8B D9 48 C7 41 ?? 0F 00 00 00 C6 01 00 8A 05 [4] EB ?? 3C 20 74 ?? 48 8B 4B ?? 44 8A 0F}
+    condition:
+        any of them
+}
+"""
 
 
 def yara_scan(raw_data):
@@ -45,72 +49,90 @@ def xor_data(data, key):
     return decoded
 
 
-def extract_config(data):
-    config_dict = {}
+def extract_ascii_string(data: bytes, offset: int, max_length=4096) -> str:
+    if offset >= len(data):
+        raise ValueError("Offset beyond data bounds")
+    end = data.find(b'\x00', offset, offset + max_length)
+    if end == -1:
+        end = offset + max_length
+    return data[offset:end].decode('ascii', errors='replace')
 
-    # Attempt to extract via old method
-    try:
-        domain = ""
-        uri = ""
+
+def parse_text(data):
+    global domain, uri
+    with suppress(Exception):
         lines = data.decode().split("\n")
+        if not lines:
+            return
         for line in lines:
             if line.startswith("http") and "://" in line:
                 domain = line
             if line.startswith("/") and line[-4] == ".":
                 uri = line
-        if domain and uri:
-            config_dict.setdefault("CNCs", []).append(f"{domain}{uri}")
-            return config_dict
-    except Exception:
-        pass
 
-    # Try with new method
 
-    # config_dict["Strings"] = []
-    pe = pefile.PE(data=data, fast_load=True)
-    image_base = pe.OPTIONAL_HEADER.ImageBase
-    domain = ""
-    uri = ""
-    botnet_id = ""
+def parse_pe(data):
+    global domain, uri, botnet_id
+    pe = None
+    image_base = 0
     last_str = ""
+    with suppress(Exception):
+        pe = pefile.PE(data=data, fast_load=True)
+        if not pe:
+            return
+        image_base = pe.OPTIONAL_HEADER.ImageBase
+        if not image_base:
+            return
     for match in yara_scan(data):
         try:
             rule_str_name, str_decode_offset = match
+            if rule_str_name.startswith("$botnet"):
+                botnet_var = struct.unpack("I", data[str_decode_offset - 4 : str_decode_offset])[0]
+                if hasattr(pe, 'OPTIONAL_HEADER'):
+                    magic = pe.OPTIONAL_HEADER.Magic
+                    if magic == 0x10b: # 32-bit
+                        botnet_offset = pe.get_offset_from_rva(botnet_var - image_base)
+                    elif magic == 0x20b: # 64-bit
+                        botnet_offset = pe.get_offset_from_rva(pe.get_rva_from_offset(str_decode_offset) + botnet_var)
+                    if botnet_offset:
+                        botnet_id = extract_ascii_string(data, botnet_offset)
             str_size = int(data[str_decode_offset + 1])
             # Ignore size 0 strings
             if not str_size:
                 continue
-
             if rule_str_name.startswith("$decode"):
                 key_rva = data[str_decode_offset + 3 : str_decode_offset + 7]
                 encoded_str_rva = data[str_decode_offset + 8 : str_decode_offset + 12]
-                # dword_rva = data[str_decode_offset + 21 : str_decode_offset + 25]
-
-            key_offset = pe.get_offset_from_rva(struct.unpack("i", key_rva)[0] - image_base)
-            encoded_str_offset = pe.get_offset_from_rva(struct.unpack("i", encoded_str_rva)[0] - image_base)
-            # dword_offset = struct.unpack("i", dword_rva)[0]
-            # dword_name = f"dword_{hex(dword_offset)[2:]}"
-
-            key = data[key_offset : key_offset + str_size]
-            encoded_str = data[encoded_str_offset : encoded_str_offset + str_size]
-            decoded_str = xor_data(encoded_str, key).decode()
-            # config_dict["Strings"].append({dword_name : decoded_str})
-
-            if last_str in ("http://", "https://"):
-                domain += decoded_str
-            elif decoded_str in ("http://", "https://"):
-                domain = decoded_str
-            elif "http" in decoded_str and "://" in decoded_str:
-                domain = decoded_str
-            elif uri == "" and decoded_str.startswith("/") and decoded_str[-4] == ".":
-                uri = decoded_str
-            elif last_str[0] == "/" and last_str[-1] == "/":
-                botnet_id = decoded_str
-
-            last_str = decoded_str
-
+                key_offset = pe.get_offset_from_rva(struct.unpack("i", key_rva)[0] - image_base)
+                encoded_str_offset = pe.get_offset_from_rva(struct.unpack("i", encoded_str_rva)[0] - image_base)
+                key = data[key_offset : key_offset + str_size]
+                encoded_str = data[encoded_str_offset : encoded_str_offset + str_size]
+                decoded_str = xor_data(encoded_str, key).decode()
+                if last_str in ("http://", "https://"):
+                    domain += decoded_str
+                elif decoded_str in ("http://", "https://"):
+                    domain = decoded_str
+                elif "http" in decoded_str and "://" in decoded_str:
+                    domain = decoded_str
+                elif uri is None and decoded_str.startswith("/") and decoded_str[-4] == ".":
+                    uri = decoded_str
+                elif last_str[0] == "/" and last_str[-1] == "/":
+                    botnet_id = decoded_str
+                last_str = decoded_str
         except Exception:
             continue
+    return
+
+
+def extract_config(data):
+    global domain, uri, botnet_id
+    domain = uri = botnet_id = None
+    config_dict = {}
+
+    if data[:2] == b'MZ':
+        parse_pe(data)
+    else:
+        parse_text(data)
 
     if domain and uri:
         config_dict.setdefault("CNCs", []).append(f"{domain}{uri}")
