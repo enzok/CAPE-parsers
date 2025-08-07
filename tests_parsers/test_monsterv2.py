f75829f0de4007d5371106af1c2cba25283c3410
doomedraven <doommedraven@gmail.com>
2025-08-07 08:11:02 +0200

    monsterv2/aurotun
    Co-Authored-By: YungBinary <93540406+YungBinary@users.noreply.github.com>

---
+++ tests_parsers/test_monsterv2.py
@@ -0,0 +1,27 @@
+from cape_parsers.CAPE.core.mosterv2 import extract_config
+
+
+def test_adaptixbeacon():
+    # Adaptix Beacon
+    with open(
+        "tests/data/malware/b842281e64924baa7c011501d7778075da412d66986e6aa65fd7d171cf074d70",
+        "rb",
+    ) as data:
+        conf = extract_config(data.read())
+        assert conf == {
+            "raw": {
+                "anti_dbg": False,
+                "anti_sandbox": False,
+                "aurotun": False,
+                "build_name": "Se2",
+                "disable_mutex": False,
+                "ip": "162.33.177.183",
+                "kx_pk": "gUpjAMXma3Kiq8lZx/2UY1kcKTlsy5CMf+/y2IuUg1A=",
+                "port": 7712,
+                "priviledge_escalation": True,
+                "seal_pk": "J2urRTT6yFi+WosGLdpP8bSyxwOPg9PTYYDVEpCZhw0=",
+                "sign_pk": "XkEb9v3B99RHP+k4LgFOvrq+WQPKPKHpIx4VKvw6IQc=",
+            },
+            "CNCs": ["tcp://162.33.177.183:7712"],
+            "build": "Se2",
+        }
