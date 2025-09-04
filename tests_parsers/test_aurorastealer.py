25e8489af9e4ad7a67ecd7d1b30e30452979f892
doomedraven <abrukhovetskyy@google.com>
2025-09-04 13:06:29 +0000

    Update test_aurorastealer.py

--- tests_parsers/test_aurorastealer.py
+++ tests_parsers/test_aurorastealer.py
@@ -4,7 +4,7 @@ def test_aurorastealer():
     with open("tests/data/malware/8da8821d410b94a2811ce7ae80e901d7e150ad3420d677b158e45324a6606ac4", "rb") as data:
         conf = extract_config(data.read())
         assert conf == {
-            'CNCs': ["77.91.85.73"],
+            'CNCs': ["tcp://77.91.85.73"],
             "build": "x64pump",
             "raw": {
                 "MD5Hash": "f29f33b296b35ec5e7fc3ee784ef68ee",
